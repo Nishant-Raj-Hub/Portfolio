c38dd12a219aede4dfc9348fc1f2b3904569a780
Nishant-Raj-Hub <nishantraj2109@gmail.com>
2024-01-29 19:30:58 +0530

    changed port

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 // server used to send send emails
-
+const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
