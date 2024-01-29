e46b1b28730d2335434b57376ffe1e637acbe982
Nishant-Raj-Hub <nishantraj2109@gmail.com>
2024-01-29 19:37:07 +0530

    changed server.js

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,10 @@ const app = express();
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "frontend", "build")));
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
 // server used to send send emails
 const PORT = process.env.PORT || 5000
 app.use(cors());
